4e5e26ac1c480693b57375af397193f0890d8874
Muhammad Yaseen <myn121myn@gmail.com>
2025-07-10 21:58:34 +0500

    News API

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 let searchInp = document.querySelector("#search");
 let mainDiv = document.querySelector(".mainDiv");
 
-let searchValue = "gaza";
+let searchValue = "pakistan";
 
 
 const fetchData = (query) => {
     mainDiv.innerHTML = "";
 
-    fetch(`https://newsapi.org/v2/everything?q=${query}&from=2025-06-03&sortBy=publishedAt&apiKey=004d10e6017345b0a33ee2c5c64a4eaa`)
+    fetch(`https://newsdata.io/api/1/latest?apikey=pub_8e9f90bf71f849e299e9af4688020c7b&q=${query}`)
         .then((data) => data.json())
         .then((data) => {
             console.log(data);
             
-            if (data.articles.length === 0) {
+            if (data.results.length === 0) {
                 mainDiv.innerHTML = `<h3 class="text-center text-danger">No news found for "${query}"</h3>`;
                 return;
             }
 
-            data.articles.map((news) => {
+            data.results.map((news) => {
                 mainDiv.innerHTML += `
                     <div class="col-md-4">
                         <div class="card h-100 shadow-sm">
-                            <img src="${news.urlToImage}" class="card-img-top" alt="News Image" style="height: 200px; object-fit: cover;">
+                            <img src="${news.image_url}" class="card-img-top" alt="News Image" style="height: 200px; object-fit: cover;">
                             <div class="card-body d-flex flex-column">
                                 <h5 class="card-title">${news.title}</h5>
                                 <p class="card-text">${news.description}</p>
-                                <a href="${news.url}" target="_blank" class="btn btn-custom mt-auto text-white">Read More</a>
+                                <a href="${news.source_url}" target="_blank" class="btn btn-custom mt-auto text-white">Read More</a>
                             </div>
                         </div>
                     </div>
